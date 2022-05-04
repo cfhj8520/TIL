c80e7e750a3b3f086c0a472243ecc6a0ad09696c
Hyunjun <cfhj8520@naver.com>
2022-05-04 16:35:33 +0900

    백준 크로아티아알파벳 통과

---
+++ src/백준/크로아티아알파벳.java
@@ -0,0 +1,24 @@
+package 백준;
+
+import java.util.Scanner;
+
+class 크로아티아알파벳 {
+    public static long solution(String str) {
+        int answer = 0;
+        String[] croatia = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        for(String cro : croatia) {
+            str = str.replaceAll(cro, "1");
+        }
+
+        return str.length();
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        String str = kb.next();
+
+        System.out.println(solution(str));
+    }
+}
