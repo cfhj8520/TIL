c1495570a35dbe63155f7c059a21daac7ace792e
cfhj8520 <cfhj8520@naver.com>
2022-05-06 15:38:24 +0900

    백준 최소최대 통과

---
+++ src/백준/최소최대.java
@@ -0,0 +1,24 @@
+package 백준;
+
+import java.util.*;
+
+public class 최소최대 {
+    public static void solution(int n, int[] arr) {
+        Arrays.sort(arr);
+
+        System.out.print(arr[0] + " " + arr[n-1]);
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int arr[] = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        solution(n, arr);
+    }
+}
