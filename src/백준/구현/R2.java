b2183a5bd4ebc16b8180f91e0948fe3e40d6444a
cfhj8520 <cfhj8520@naver.com>
2022-05-07 23:41:34 +0900

    백준 R2 통과

---
+++ src/백준/구현/R2.java
@@ -0,0 +1,14 @@
+package 백준.구현;
+
+import java.util.*;
+
+public class R2 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        int a = kb.nextInt();
+        int b = kb.nextInt();
+
+        System.out.println(b*2-a);
+    }
+}
